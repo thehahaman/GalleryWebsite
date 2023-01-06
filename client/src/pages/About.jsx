4a43b506f33677c15eac919ef0310466094ffe0b
thehahaman <gabousetta333@gmail.com>
2023-01-06 15:26:06 -0700

    eedit

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -10,18 +10,22 @@ function About() {
       <Navbar />
 
       <div className='about'>
-        <div className="profile">
-          <img className='profileImage' src='images/hikePhoto.png' alt='profile' />
-          <h1> George Abouseta </h1>
+        <div className="me">
+          <div className="profile">
+            <img className='profileImage' src='images/hikePhoto.png' alt='profile' />
+            <h1> George Abouseta </h1>
+          </div>
+
+          <div className="aboutLinks">
+            <a href="https://www.linkedin.com/in/george-a-522a7211b/"> <img className='linkPhoto' src='images/linkedIn.png' alt='Linked In' /> </a>
+            <a href="https://www.instagram.com/george_abosetta/"><img className='instagramPhoto' src='images/instagramLogo.png' alt='Instagram' /> </a>
+          </div>
+          <div className="gmail">
+            <img className='linkPhoto' src='images/gmailLogo.png' alt='Gmail' /> <span>gabousetta333@gmail.com</span>
+          </div>
+
         </div>
 
-        <div className="aboutLinks">
-          <a href="https://www.linkedin.com/in/george-a-522a7211b/"> <img className='linkPhoto' src='images/linkedIn.png' alt='Linked In' /> </a>
-          <a href="https://www.instagram.com/george_abosetta/"><img className='instagramPhoto' src='images/instagramLogo.png' alt='Instagram' /> </a>
-        </div>
-        <div className="gmail">
-          <img className='linkPhoto' src='images/gmailLogo.png' alt='Gmail' /> <span>gabousetta333@gmail.com</span>
-        </div>
 
 
       </div>
