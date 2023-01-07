75b2548c30b48bdd8ada458bbb5d675aeb1a6166
thehahaman <gabousetta333@gmail.com>
2023-01-06 17:30:14 -0700

    about me section updated

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import Navbar from '../components/Navbar'
 import '../components/Navbar.css'
 import './about.css'
@@ -14,6 +13,7 @@ function About() {
           <div className="profile">
             <img className='profileImage' src='images/hikePhoto.png' alt='profile' />
             <h1> George Abouseta </h1>
+            <h6> Software developer </h6>
           </div>
 
           <div className="aboutLinks">
@@ -27,7 +27,21 @@ function About() {
         </div>
 
         <div className="aboutme">
-          this is an example of about me
+          <p className='shortDesc'>Hello! My name is George and I am a recent computer science graduate.
+            I have always been interested in coding and enjoy spending my free time
+            coming up with innovative solutions for problems, making small video games, and cooking.
+            I taught Javascript and C# to kids in Calgary and i'm very interested in computer networking and software development.
+            I am also a Cisco Certified Network Associate.
+          </p>
+
+          <div className='shortFacts'>
+            <p><b>Favourite stack</b>: FERN</p>
+            <p><b>Favourite snack</b>: M&Ms</p>
+            <p><b>Location</b>: Calgary, Alberta</p>
+            <p><b>Language preference</b>: Java</p>
+          </div>
+
+
         </div>
 
       </div>
